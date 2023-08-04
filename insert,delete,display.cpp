901d1757664fe4e24b80b282b70b9caa6bfb95e1
Shuraim_ms <123939339+Shuraim04@users.noreply.github.com>
2023-08-04 08:16:59 +0530

    Add files via upload

--- insert,delete,display.cpp
+++ insert,delete,display.cpp
@@ -1,7 +1,5 @@
 #include <stdio.h>
-
 #define MAX_SIZE 100
-
 // Function to display the array elements
 void display(int arr[], int size) {
     printf("Array: ");
@@ -10,19 +8,16 @@ void display(int arr[], int size) {
     }
     printf("\n");
 }
-
 // Function to insert an element at a given position
 int insert(int arr[], int *size, int element, int position) {
     if (*size >= MAX_SIZE) {
         printf("Array is full, cannot insert element.\n");
         return 0;
     }
-
     if (position < 0 || position > *size) {
         printf("Invalid position for insertion.\n");
         return 0;
     }
-
     for (int i = *size; i > position; i--) {
         arr[i] = arr[i - 1]; // Shift elements to the right
     }
@@ -30,41 +25,33 @@ int insert(int arr[], int *size, int element, int position) {
     (*size)++;
     return 1;
 }
-
 // Function to delete an element from a given position
 int deleteElement(int arr[], int *size, int position) {
     if (*size <= 0) {
         printf("Array is empty, cannot delete element.\n");
         return 0;
     }
-
     if (position < 0 || position >= *size) {
         printf("Invalid position for deletion.\n");
         return 0;
     }
-
     for (int i = position; i < *size - 1; i++) {
         arr[i] = arr[i + 1]; // Shift elements to the left
     }
     (*size)--;
     return 1;
 }
-
 int main() {
     int arr[MAX_SIZE];
     int size = 0;
-
     int choice = -1, element, position;
-
     while (choice != 4) {
         printf("Enter your choice:\n");
         printf("1. Insert an element\n");
         printf("2. Delete an element\n");
         printf("3. Display the array\n");
         printf("4. Exit\n");
-
         scanf("%d", &choice);
-
         if (choice == 1) {
             printf("Enter the element to insert: ");
             scanf("%d", &element);
@@ -87,7 +74,5 @@ int main() {
             printf("Invalid choice. Please try again.\n");
         }
     }
-
     return 0;
 }
-
